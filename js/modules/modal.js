244ec26b249c2fe786a04d9416cfc5c3f0efe7d7
Vitor <frete497@gmail.com>
2022-06-20 17:28:09 -0400

    ajustes return

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -33,7 +33,7 @@ export default class Modal {
   init() {
     if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
       this.addModalEvents();
-      return this;
     }
+    return this;
   }
 }
